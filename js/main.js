673e17cffd623f94640c64302eb16f432c8e83ff
Arthur Nery <arthurtobbyy@gmail.com>
2026-06-01 22:12:00 -0300

    feat(navbar): adiciona estrutura DOMContentLoaded e seletores do hamburger

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  /* MENU HAMBURGER */
+  var hamburger = document.getElementById('hamburger');
+  var navLinks  = document.getElementById('navLinks');
+})
+
